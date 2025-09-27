ddf315100214ebf63521aa77f1e18f199b3e5261
Lucas Noronha <lucas.noronha.gois@hotmail.com>
2025-09-26 23:38:57 -0300

    railway fix

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -3,5 +3,23 @@ from .classification_service import ClassificationService
 from .nlp_service import NLPService
 from .ai_service import AIService
 from .file_service import FileService
+from .historico_service import HistoricoService
+from .prompt_service import PromptService
+from .hybrid_prompt_service import HybridPromptService
+from .free_ai_service import FreeAIService
+from .simple_context_classification import SimpleContextClassification
+from .user_service import UserService
 
-__all__ = ["EmailService", "ClassificationService", "NLPService", "AIService", "FileService"]
+__all__ = [
+    "EmailService", 
+    "ClassificationService", 
+    "NLPService", 
+    "AIService", 
+    "FileService",
+    "HistoricoService",
+    "PromptService",
+    "HybridPromptService",
+    "FreeAIService",
+    "SimpleContextClassification",
+    "UserService"
+]
